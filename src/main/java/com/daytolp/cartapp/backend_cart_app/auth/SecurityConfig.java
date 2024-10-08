bdb945b409f2bf5f114269e02bb08178be40c2cf
Juan Fidel <yashirorko@gmail.com>
2024-10-08 14:14:11 -0600

    se agrega cors

--- src/main/java/com/daytolp/cartapp/backend_cart_app/auth/SecurityConfig.java
+++ src/main/java/com/daytolp/cartapp/backend_cart_app/auth/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.daytolp.cartapp.backend_cart_app.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -11,10 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import com.daytolp.cartapp.backend_cart_app.auth.filters.JWTAuthenticationFilter;
 import com.daytolp.cartapp.backend_cart_app.auth.filters.JWTAuthorizationFilter;
 import com.daytolp.cartapp.backend_cart_app.auth.providers.JwtProvider;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -28,6 +35,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(request -> 
             request.requestMatchers(HttpMethod.GET, "/users").permitAll()
+            .requestMatchers(HttpMethod.GET, "/products").permitAll()
             .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("USER", "ADMIN")
             .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
             .requestMatchers("/users/**").hasRole("ADMIN")
@@ -36,6 +44,7 @@ public class SecurityConfig {
         .addFilter(new JWTAuthorizationFilter(authenticationConfiguration.getAuthenticationManager(), jwtProvider))
         .csrf(config -> config.disable())
         .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .build();
     }
 
@@ -47,6 +56,26 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager() throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+    
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 
 }
